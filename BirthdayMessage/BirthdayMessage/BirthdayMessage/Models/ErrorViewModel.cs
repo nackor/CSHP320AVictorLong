cc7fdb9f8c5f8e2cc84ee9d39826a6300f63ff68
Victor Long <nackor91@gmail.com>
2022-04-25 20:34:27 -0700

    made project

---
+++ BirthdayMessage/BirthdayMessage/BirthdayMessage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BirthdayMessage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ BirthdayMessage/BirthdayMessage/BirthdayMessage/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayMessage.Models
+{
+    public class Message
+    {
+        [Required(ErrorMessage = "Please enter who the message is from")]
+        public string From { get; set; }
+        [Phone]
+        [Required(ErrorMessage = "Please enter your phone")]
+        public string? Phone { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Please enter your email")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your willattend")]
+        public bool? WillAttend { get; set; }
+    }
+}
