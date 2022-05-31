de2d931a5e2a85c1b35713834c86d5878ccb1ac0
Victor Long <nackor91@gmail.com>
2022-05-30 20:02:29 -0700

    added db

---
+++ Homework3/Homework3/Homework3/Db/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework3.Db
+{
+    public partial class Contact
+    {
+        public Contact(int ID, string Name, string Email, string PhoneType, string Number, int Age, string Note, DateTime Date)
+        {
+            ContactId = ID;
+            ContactName = Name;
+            ContactEmail = Email;
+            ContactPhoneType = PhoneType;
+            ContactPhoneNumber = Number;
+            ContactAge = Age;
+            ContactNotes = Note;
+            ContactCreatedDate = Date;
+        }
+        public int ContactId { get; set; }
+        public string ContactName { get; set; } = null!;
+        public string? ContactEmail { get; set; }
+        public string? ContactPhoneType { get; set; }
+        public string? ContactPhoneNumber { get; set; }
+        public int ContactAge { get; set; }
+        public string? ContactNotes { get; set; }
+        public DateTime ContactCreatedDate { get; set; }
+    }
+}
---
+++ Homework3/Homework3/Homework3/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Homework3.Db;
+
+
+Console.WriteLine("Hello, World!");
+ContactsContext contactsContext = new ContactsContext();
+
+Contact contactOne = new Contact();
+
